923e87b4ad797cd0d1cfb3592ad0999645bb5418
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2020-11-13 13:44:32 +0100

    update readme, appspec, code

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
-# MorningRep_AttrPlots
+# Morning Report pdf Attribute Plots
+MoveApps
+
+Github repository: *github.com/movestore/MorningRep_AttrPlots*
 
 ## Description
 This App creates a multipage pdf downloadable file with time-plots of up to five selected data attributes for each individual track. A reference time and (back) time duration have to be provided. So that you can get an overview of how your animals (and tags) were doing. 
 
 ## Documentation
-gives out multipage pdfs with individual Plots of requrested Attributes (max 5)
+A multipage pdf is created of up to 5 user-defined data attributes across time. The plotted time window is defined by the reference timestamp (either user-defined or by default NOW) and the time duration that defines how long before the reference timestamp the x-axis of the plots shall start.
+
+### Input data
+moveStack in Movebank format
+
+### Output data
+moveStack in Movebank format
+
+### Artefacts
+`MorningReport_attribPlots.pdf`: Artefact pdf with a multiple attribute plots on each page showing the time series of the selected attributes for one animal.
+
+### Parameters 
+`time_now`: reference timestamp towards which all analyses are performed. Generally (and by default) this is NOW, especially if in the field and looking for one or the other animal or wanting to make sure that it is still doing fine. When analysing older data sets, this parameter can be set to other timestamps so that the to be plotted data fall into a selected back-time window. 
+
+`time_dur`: time duration into the past that the attributes have to be plotted for. So, if the time duration is selected as 7 days then the x-axis ranges from the reference timestamp to 7 days before it. Unit: days
+
+`attribs`: the user has to provide the data attributes that shall be plotted for the defined time window. They have to be correctly spelled and comma-separated. Example: "tag_voltage, location_lat".
+
+### Null or error handling:
+**Parameter `time_now`:** If this parameter is left empty (NULL) the reference time is set to NOW. The present timestamp is extracted in UTC from the MoveApps server system.
+
+**Parameter `time_dur`:** If this parameter is left empty (NULL) then by default 10 days is used. A respective warning is given.
+
+**Parameter `attribs`:** If no attributes are defined (NULL) or none of the given attributes relate to any column names of the data set, then no pdf artefact is created. If more than 5 valid attributes are listed a warning is given, but the plotting is still done; the plots might be cut off on the pdf pages.
+
+**Artefact:** If there are no locations of any animals in the defined time window, a warning is given and no pdf artefact created.
+
+**Data:** The data are not manipulated in this App, but plotted in a downloadable pdf. So that a possible Workflow can be continued after this App, the input data set is returned.
--- RFunction.R
+++ RFunction.R
@@ -15,7 +15,7 @@ rFunction = function(time_now=NULL, attribs=NULL, time_dur=NULL, data, ...) { #d
   
   if (is.null(attribs))
   {
-    logger.info("Warning! You did not provide any attribute names to be plotted. The function only returns the input data set. No plots will be generated.")
+    logger.info("Warning! You did not provide any attribute names to be plotted. The function returns the input data set. No plots will be generated.")
   } else
   {
     attribs_list <- strsplit(attribs,",")[[1]]
@@ -27,7 +27,7 @@ rFunction = function(time_now=NULL, attribs=NULL, time_dur=NULL, data, ...) { #d
     {
       time_dur <- 10
       logger.info("You did not provide a time duration for your plot. It is defaulted by 10 days.")
-    }  else  time_dur <- as.numeric(time_dur)
+    }  #else  time_dur <- as.numeric(time_dur)
     time0 <- time_now - as.difftime(time_dur,units="days")
     
     attribs_ok <- attribs_list[attribs_list %in% names(data)]
@@ -35,10 +35,12 @@ rFunction = function(time_now=NULL, attribs=NULL, time_dur=NULL, data, ...) { #d
     attribs_error <- attribs_list[!attribs_list %in% names(data)]
     if (length(attribs_error)>0) logger.info(paste0("Warning! Your defined attributes: ",paste(attribs_error,collapse=", ")," do not exist in the data set. They will not be plotted."))
     
+    if (len>5) logger.info("Warning! You have selected more than 5 valid attributes to plot. This might lead to strange cutoffs on your pdf pages.")
     if (len>0)
     {
       logger.info(paste0("Your defined attributes: ",paste(attribs_ok,collapse=", ")," exist in the data set and will be plotted."))
       g <- list()
+      ids_g <- character()
       k <- 1
       for (i in seq(along=ids))
       {
@@ -54,17 +56,21 @@ rFunction = function(time_now=NULL, attribs=NULL, time_dur=NULL, data, ...) { #d
             facet_grid(variable ~ ., scales = "free_y") +
             labs(title = paste("individual:",ids[i])) +
             theme(plot.margin=grid::unit(c(2,2,2+3*(5-len),2), "cm")) #max 5 attributes to plot
+          ids_g <- c(ids_g,ids[i])
           k <- k+1
           
         } else logger.info(paste0("There are no locations available in the requested time window for individual ",ids[i]))
       }
       
-      gp  <- marrangeGrob(g, nrow = 1, ncol = 1)
-      ggsave(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"MorningReport_attribPlots.pdf"), gp, width = 21, height = 29.7, units = "cm")
-      #ggsave("MorningReport_attribPlots.pdf",gp, width = 21, height = 29.7, units = "cm")
-      
-    } else logger.info("The provided attributes are not available in the given data set, thus no plots can be generated. The input data set is returned.")
-
+      if (length(ids_g)>0)
+      {
+        logger.info(paste0("Maps are produced for the individuals ",paste(ids_g,collapse=", "),", which have data in the requested time window."))
+        gp  <- marrangeGrob(g, nrow = 1, ncol = 1)
+        ggsave(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"MorningReport_attribPlots.pdf"), gp, width = 21, height = 29.7, units = "cm")
+        #ggsave("MorningReport_attribPlots.pdf",gp, width = 21, height = 29.7, units = "cm")
+      } else logger.info ("None of the individuals have data in the requested time window. Thus, no plots (pdf artefact) is generated.")
+        
+    } else logger.info("The provided attributes are not available in the given data set, thus no plots (pdf artefact) can be generated.")
   }
   
   return(data)
