ed5ad36b619e5c7b35e0510224f080b65d089cf2
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2021-05-12 10:50:27 +0200

    ,format="%Y-%m-%dT%H:%M:%OSZ"

--- RFunction.R
+++ RFunction.R
@@ -11,7 +11,7 @@ Sys.setenv(tz="GMT")
 
 rFunction = function(time_now=NULL, attribs=NULL, time_dur=NULL, data, ...) { #dont give id selection option, but decide that only plot those with data in the time_dur window
   
-  if (is.null(time_now)) time_now <- Sys.time() else time_now <- as.POSIXct(time_now)
+  if (is.null(time_now)) time_now <- Sys.time() else time_now <- as.POSIXct(time_now,format="%Y-%m-%dT%H:%M:%OSZ",tz="GMT")
   
   if (is.null(attribs))
   {
